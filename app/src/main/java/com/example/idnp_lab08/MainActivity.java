8fa3b78d82601ed05afb70df47e0e6f1467c5678
NeiNeiP1 <72216162+NeiNeiP1@users.noreply.github.com>
2021-11-16 19:35:25 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/idnp_lab08/MainActivity.java
+++ app/src/main/java/com/example/idnp_lab08/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.idnp_lab08;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
+import android.content.Intent;
 import android.os.Bundle;
 
 import java.util.ArrayList;
@@ -29,10 +29,21 @@ public class MainActivity extends AppCompatActivity {
         rutas.add(new Ruta("3 de Octubre", "B", "5:00 AM - 9:00 PM"));
         rutas.add(new Ruta("3 de Octubre", "D", "5:00 AM - 9:00 PM"));
 
-        ListAdapter listAdapter = new ListAdapter(rutas, this);
+         ListAdapter listAdapter = new ListAdapter(rutas, this, new ListAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(Ruta item) {
+                moveToDescription(item);
+            }
+        });
         RecyclerView recyclerView = findViewById(R.id.recyclerRutas);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(listAdapter);
+    }
+    //Al acudir el método mostrará otra actividad, este pasa el objeto con todo,
+    public void  moveToDescription(Ruta item){
+        Intent inten =new Intent(this, DescripcionActivity.class);
+        inten.putExtra("Ruta", item);
+        startActivity(inten);
     }
 }
